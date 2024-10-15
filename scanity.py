dc7170439b757d5fd8d7f80ec248a98963c640c8
zE0nyx <77500868+zE0nyx@users.noreply.github.com>
2024-10-15 20:14:46 +0530

    Update scanity.py

--- scanity.py
+++ scanity.py
@@ -4,6 +4,7 @@ from scanner import *
 
 ascii_banner = pyfiglet.figlet_format("Scanity")
 print(ascii_banner)
+print("- - - - - - - - - - - - - - - - - - - - - - - -")
 
 
 def run_scan(target, args):
